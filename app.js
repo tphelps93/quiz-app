82a790c66dee57b33e03c119a272c0a576726dfc
Taylor Phelps <tphelps1993@gmail.com>
2020-09-16 21:48:58 -0400

    added functionality in the js and css

--- app.js
+++ app.js
@@ -5,30 +5,36 @@ const store = {
   // 5 or more questions are required
   questions: [
     {
-      question: 'What color is broccoli?',
+      question: 'What is the distance to the moon from Earth?',
       answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
+        '238,900 miles',
+        '500,000 miles',
+        '20 miles',
+        '509,523 miles'
       ],
-      correctAnswer: 'green'
+      correctAnswer: '238,900 miles'
     },
     {
-      question: 'What is the current year?',
+      question: 'What type of volcanoes are on the surface of the moon, Titan?',
       answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
+        'Lava Domes',
+        'Cryo Volcano',
+        'Composite Volcanoes',
+        'Shield Volcanoes'
       ],
-      correctAnswer: '2019'
+      correctAnswer: 'Cryo Volcano'
     }
   ],
-  quizStarted: false,
+  quizStarted: true,
   questionNumber: 0,
   score: 0
 };
+
+function getQuestions() {
+  let number = store.questionNumber;
+  let nextQuestion = store.questions[number];
+  return nextQuestion;
+}
 
 /**
  * 
@@ -48,7 +54,6 @@ const store = {
 /********** TEMPLATE GENERATION FUNCTIONS **********/
 
 // These functions return HTML templates
-
 /********** RENDER FUNCTION(S) **********/
 
 // This function conditionally replaces the contents of the <main> tag based on the state of the store
@@ -56,3 +61,130 @@ const store = {
 /********** EVENT HANDLER FUNCTIONS **********/
 
 // These functions handle events (submit, click, etc)
+
+// generate HTML functions
+function generateStartPage() {
+  // Generates start page
+  const template = `<button id='start-quiz' class='next-question'>START</button>`;
+
+  render(template);
+}
+
+
+// make sure to have function update the state
+
+function generateQuestionPage() {
+  // Generates question page
+
+  let questionNum = store.questionNumber;
+  let question = getQuestions();
+
+  let template = `<form class='container'>
+      <h2> Question ${questionNum + 1}</h2>
+      <p>${question.question}</p>
+      <ul>
+        <li><input type='radio' value='${question.answers[0]}' name='quiz-question'/><label>${question.answers[0]}</label></li>
+        <li><input type='radio' value='${question.answers[1]}' name='quiz-question'/><label>${question.answers[1]}</label></li>
+        <li><input type='radio' value='${question.answers[2]}' name='quiz-question'/><label>${question.answers[2]}</label></li>
+        <li><input type='radio' value='${question.answers[3]}' name='quiz-question'/><label>${question.answers[3]}</label></li>
+        <li><button type='submit' class='submit-answer'>Submit Answer</button></li>
+      </ul>
+
+      
+
+      <p>${questionNum} out of ${store.questions.length}</p>
+
+      <p> Should count number of correct questions and number of wrong questions </p>
+ 
+    
+    </form>
+  `
+  render(template);
+}
+
+function generateAnswerScreen() {
+  let userAnswer = $('input[name="quiz-question"]:checked').val();
+  console.log(getQuestions());
+  let question = getQuestions();
+  let correctAnswer = question.correctAnswer;
+  let score = store.score;
+  let template;
+
+  
+  if (correctAnswer === userAnswer) {
+    store.score += 1;
+    store.questionNumber += 1;
+    template = `<h2> Correct </h2>`
+  } else {
+    store.questionNumber += 1;
+    template = `<h2> Incorrect </h2>`
+  }
+  render(template);
+}
+
+
+
+
+
+// callback function 
+function handleQuizApp() {
+  render();
+  generateStartPage();
+  startQuiz();
+  checkAnswer();
+  nextQuestion();
+  restartQuiz();
+}
+
+
+
+
+// when the page loads, call `handleShoppingList`
+$(handleQuizApp);
+
+function render(state) {
+  $('body').html(`${state}`)
+}
+
+// Event Handlers
+
+
+function nextQuestion() {
+  $('body').on('click', '.next-question', function(event) {
+    event.preventDefault();
+    generateQuestionPage();
+  })
+  // event for handling click to go to the next question
+
+  console.log('nextQuestion ran');
+}
+
+// *****For Chris --- When the 'START' button is clicked, the question view is rendered.
+function startQuiz() {
+  $('body').on('click', '#start-button' ,function(event) {
+    event.preventDefault();
+    nextQuestion();
+    // display first question
+    // display answers
+    // radio button
+    // display submit answer button
+    // display counter for questions answered vs questions unanswered
+  })
+  // When the user presses the "START" button, display first question
+}
+
+function checkAnswer() {
+  // event handler for calculating right or wrong answer
+  $('body').on('click', '.submit-answer', function(event) {
+    event.preventDefault();
+    generateAnswerScreen();
+  })
+  
+  console.log('checkAnswer ran');
+}
+
+function restartQuiz() {
+  // even to restart the quiz from the beginning
+
+  console.log('restartQuiz ran');
+}
--- data.js
+++ data.js
@@ -1,83 +1,84 @@
 /*--- Step 1 - Defining global variables ---*/
-
-var questionsArray = [
+let questions = [
     //Question 1
         {
             questionText: 'What is the distance to the moon from Earth',
             questionChoices: ['238,900 miles', '500,000 miles', '20 miles', '509,523 miles'],
             questionCorrectChoice: 1,
-            correctDetails: '238,900 miles'
+            correctDetails: '238,900 miles',
+            number: 1
         },
     
     //Question 2
         {
             questionText: 'What type of volcanoes are on the surface of the moon Titan?',
-            questionChoices: ['Lava Domes' 'Cryo Volcano', 'composite volcanoes', 'shield volcanoes'],
+            questionChoices: ['Lava Domes', 'Cryo Volcano', 'composite volcanoes', 'shield volcanoes'],
             questionCorrectChoice: 2,
-            correctDetails: '93.436 million miles'
-        },
+            correctDetails: '93.436 million miles',
+            number: 2
+        } //,
     
-    //Question 3
-        {
-            questionText: 'What is the distance to Pluto from Earth',
-            questionChoices: ['200.53 billion miles', '50.8 million miles', '3.1259 billion miles', '500 trillion miles'],
-            questionCorrectChoice: 0,
-            correctDetails: '3.1259 billion miles'
-        },
+//     //Question 3
+//         {
+//             questionText: 'What is the distance to Pluto from Earth',
+//             questionChoices: ['200.53 billion miles', '50.8 million miles', '3.1259 billion miles', '500 trillion miles'],
+//             questionCorrectChoice: 0,
+//             correctDetails: '3.1259 billion miles'
+//         },
     
-    //Question 4
-    {
-        questionText: 'How many days will it take to get to the Sun from Earth',
-        questionChoices: ['19 years', '4 days', '12 years' , '16 years'],
-        questionCorrectChoice: 2,
-        correctDetails: '19 years'
-    },
+//     //Question 4
+//     {
+//         questionText: 'How many days will it take to get to the Sun from Earth',
+//         questionChoices: ['19 years', '4 days', '12 years' , '16 years'],
+//         questionCorrectChoice: 2,
+//         correctDetails: '19 years'
+//     },
 
-//Question 5
-    {
-        questionText: 'How many Planets are in our Solar System',
-        questionChoices: [12, 8 , 9, 7],
-        questionCorrectChoice: 2,
-        correctDetails: "8 but you may think 9 if you don't believe NASA"
-    },
+// //Question 5
+//     {
+//         questionText: 'How many Planets are in our Solar System',
+//         questionChoices: [12, 8 , 9, 7],
+//         questionCorrectChoice: 2,
+//         correctDetails: "8 but you may think 9 if you don't believe NASA"
+//     },
 
-//Question 6
-    {
-        questionText: 'What is the distance from the Earth to Venus',
-        questionChoices: ['162 millon miles', '102 million miles', '204 million miles', '64 million miles' ],
-        questionCorrectChoice: 1,
-        correctDetails: '162 million miles (261 million kilometers)'
-    },
+// //Question 6
+//     {
+//         questionText: 'What is the distance from the Earth to Venus',
+//         questionChoices: ['162 millon miles', '102 million miles', '204 million miles', '64 million miles' ],
+//         questionCorrectChoice: 1,
+//         correctDetails: '162 million miles (261 million kilometers)'
+//     },
 
-//Question 7
-    {
-        questionText: 'When was the last time the U.S. went to the moon',
-        questionChoices: ['Jan 6th 1974', 'Oct 12th, 1982' , 'December 14, 1972' , 'June 4th, 1969'],
-        questionCorrectChoice: 2,
-        correctDetails: 'December 14, 1972 Apollo 17 became the last manned mission to the Moon'
-    },
+// //Question 7
+//     {
+//         questionText: 'When was the last time the U.S. went to the moon',
+//         questionChoices: ['Jan 6th 1974', 'Oct 12th, 1982' , 'December 14, 1972' , 'June 4th, 1969'],
+//         questionCorrectChoice: 2,
+//         correctDetails: 'December 14, 1972 Apollo 17 became the last manned mission to the Moon'
+//     },
 
-//Question 8
-    {
-        questionText: 'what is the farthest Planet from the sun',
-        questionChoices: ['Pluto' , 'Neptune' , 'Saturn' , 'Mars' ],
-        questionCorrectChoice: 2,
-        correctDetails: 'Neptune - 2.83 billion miles . But Pluto really would be if it was a planet still.'
-    },
+// //Question 8
+//     {
+//         questionText: 'what is the farthest Planet from the sun',
+//         questionChoices: ['Pluto' , 'Neptune' , 'Saturn' , 'Mars' ],
+//         questionCorrectChoice: 2,
+//         correctDetails: 'Neptune - 2.83 billion miles . But Pluto really would be if it was a planet still.'
+//     },
     
-    //Question 9
-        {
-            questionText: 'What is the biggest dump in space?',
-            questionChoices: ['Earth', 'Jupiter', 'The Sun', 'The Moon'],
-            questionCorrectChoice: 1,
-            correctDetails: 'Earth'
-        },
+//     //Question 9
+//         {
+//             questionText: 'What is the biggest dump in space?',
+//             questionChoices: ['Earth', 'Jupiter', 'The Sun', 'The Moon'],
+//             questionCorrectChoice: 1,
+//             correctDetails: 'Earth'
+//         },
     
-//Question 10
-{
-    questionText: 'what is the cost of  NASA space suit?',
-    questionChoices: ['$12,000' , '$46,000' , '$789,000' , '$12,000,000'],
-    questionCorrectChoice: 4,
-    correctDetails: '$12,000,000'
-}
-    ];
+// //Question 10
+// {
+//     questionText: 'what is the cost of  NASA space suit?',
+//     questionChoices: ['$12,000' , '$46,000' , '$789,000' , '$12,000,000'],
+//     questionCorrectChoice: 4,
+//     correctDetails: '$12,000,000'
+// }
+    ]
